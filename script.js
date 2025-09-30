e56fe2027abdc7fb4ea3ffe87c720eb5b10f8546
Carol Tang <catang824@gmail.com>
2025-09-30 22:30:54 +1300

    progress MoodBlanketTracker#2

---
+++ script.js
@@ -0,0 +1,25 @@
+const blanket = document.getElementById('blanket')
+const select = document.getElementById('moodColour')
+const intensitySelect = document.getElementById('moodIntensity')
+const button = document.getElementById('addMood')
+const legend = document.getElementById('legend')
+
+const moods = {
+  Anxious: '#692544',
+  Angry: '#ff0000',
+  Calm: '#6fc5fa',
+  Confident: '#fecb00',
+  Depressed: '#41648d',
+  Disappointed: '#dfddab',
+  Excited: '#d81b5c',
+  Happy: '#ffde21',
+  Hopeful: '#ffa0b3',
+  Loved: '#ff8da1',
+  Neutral: '#ffffff',
+  Overwhelmed: '#d52121',
+  Playful: '#e8779e',
+  Productive: '#5f609c',
+  Stressed: '#3b060f',
+  Tired: '#596e79',
+  Thoughtful: '#7d79be',
+}
